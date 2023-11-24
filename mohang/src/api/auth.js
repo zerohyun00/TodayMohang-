1815eeedfc5ecf6331780b5aec5f058923b0e628
zerohyun <kyhh39@naver.com>
2023-11-25 00:36:38 +0900

    [FIX] 회원가입 로직 수정

--- mohang/src/api/auth.js
+++ mohang/src/api/auth.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../static";
 
-export function HandleRegister(userData) {
-  axios
-    .post(`${BASE_URL}/user/signup`, userData )
-    .then((res) => {
-      console.log(res);    
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
 export function HandleLogin(userData) {
   axios.post(`${BASE_URL}/user/login`, userData)
   .then((res) => {
