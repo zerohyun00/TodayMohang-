2170c00e13d86dede28f6fb21f5287b1a9487d6f
bishoe01 <bishoe01@gmail.com>
2023-11-25 10:17:25 +0900

    [ADD] react-calendar - in UI 적용

--- mohang/src/pages/CalendarPage.jsx
+++ mohang/src/pages/CalendarPage.jsx
@@ -2,32 +2,35 @@ import React from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../styles/calendar.css";
+import Layout from "../layout/Layout";
 
 function CalendarPage() {
-  // An example events object, you would replace this with your actual event data
   const events = {
     "2023-11-13": 2,
     "2023-11-18": 2,
-    // Add more dates and event counts as needed
+  };
+
+  const toLocalISOString = (date) => {
+    const offset = date.getTimezoneOffset();
+    const localDate = new Date(date.getTime() - offset * 60 * 1000);
+    return localDate.toISOString().split("T")[0];
   };
 
   const tileContent = ({ date, view }) => {
-    // Convert the date to a simple string format 'yyyy-mm-dd'
-    const dateString = date.toISOString().split("T")[0];
-    // Check if there are events for this date
+    const dateString = toLocalISOString(date);
     if (view === "month" && events[dateString]) {
-      // Return a div with the number of events for this date
       return <div className="events-number">{events[dateString]}</div>;
     }
   };
 
+  const formatMonthYear = (locale, date) => {
+    return toLocalISOString(date).slice(0, 7); // YYYY-MM format
+  };
+
   return (
-    <div>
-      <Calendar
-        tileContent={tileContent}
-        // Other props as needed
-      />
-    </div>
+    <Layout title={"행사 캘린더"}>
+      <Calendar tileContent={tileContent} formatMonthYear={formatMonthYear} />
+    </Layout>
   );
 }
 
