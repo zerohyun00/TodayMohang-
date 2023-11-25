8d84bfa5140c2eb18c52f11b0aecb8389d5cc6c4
bishoe01 <bishoe01@gmail.com>
2023-11-25 11:31:28 +0900

    [FEAT] 대학인증 토글

--- mohang/src/pages/Bookmark.jsx
+++ mohang/src/pages/Bookmark.jsx
@@ -7,7 +7,7 @@ function Bookmark() {
       <div className="flex flex-col justify-center items-center h-[60vh]">
         <p className="flex flex-col items-center text-xl text-gray1">
           <span>즐겨찾기에 담은</span>
-          <span>행사가 있어요</span>
+          <span>행사가 없어요</span>
         </p>
         <CTABtn title={"행사 보러가기"} />
       </div>
--- mohang/src/pages/Mypage.jsx
+++ mohang/src/pages/Mypage.jsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import Profile from "../assets/images/profile.jpeg";
 import { FaChevronRight } from "react-icons/fa";
 import CTABtn from "../components/CTABtn";
+import { Link } from "react-router-dom";
 
 function Mypage() {
   const [univAuth, setUnivAuth] = useState(false);
+
   return (
     <div className="flex flex-col items-center relative h-[100vh] px-5 pt-[100px] gap-5">
-      <button
+      <Link
+        to={univAuth ? "" : "/univAuth"}
         className={`${
           univAuth ? "bg-primary" : "bg-gray2"
-        } w-[140px] rounded-full text-xs py-1 mb-[30px]`}
+        } w-[140px] rounded-full text-center text-xs py-1 mb-[30px]`}
       >
-        대학인증하기
-      </button>
+        {univAuth ? "대학인증 완료" : "대학 인증하기"}
+      </Link>
       <h2 className="semibold text-xl ">사자는 구구</h2>
       <img
         src={Profile}
@@ -21,9 +24,9 @@ function Mypage() {
         className="w-[100px] h-[100px] rounded-full ring-2 ring-primary"
       />
       <CTABtn title={"나의 행사 보러가기"} />
-      <button className="fixed bottom-10 text-gray1 text-xs">
+      <Link to={"/login"} className="fixed bottom-10 text-gray1 text-xs">
         로그아웃하기
-      </button>
+      </Link>
     </div>
   );
 }
